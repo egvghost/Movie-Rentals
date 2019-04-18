33aa4c3a7f8b937dbbd056d2a415daabb110b528
egvghost <egvghost@gmail.com>
2019-04-18 17:44:32 -0300

    genres migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_191234) do
+ActiveRecord::Schema.define(version: 2019_04_18_202654) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
