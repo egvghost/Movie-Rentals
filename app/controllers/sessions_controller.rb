ce1de7f6f6c876205b9ab64f8a62af8ea82565fa
egvghost <egvghost@gmail.com>
2019-05-21 16:01:52 -0300

    clase 11

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    pwd = params[:session][:password]
+    user = User.find_by(email: email)
+    if user && user.authenticate(pwd) #primero chequea que exista el user y después autentica
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      redirect_to login_url
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
