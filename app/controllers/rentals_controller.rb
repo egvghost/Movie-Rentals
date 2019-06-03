3af5de3748a3d453704c67fca615667bfeb71ef8
egvghost <egvghost@gmail.com>
2019-06-03 16:40:23 -0300

    Clase 14

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,6 @@
 class RentalsController < ApplicationController
   before_action :set_movie, only: [:new, :create]
+  before_action :verify_if_admin_and_redirect_with_error_message_if_yes, only: [:new, :create]
 
   def index
   end
@@ -9,29 +10,40 @@ class RentalsController < ApplicationController
   end
 
   def new
-    @rental = Rental.new
-    @rental.valid_from = Time.now
-    @rental.valid_to = @rental.valid_from + 2.days
+    @rental = generate_new_rental
   end
 
   def create
-    @rental = Rental.new
-    @rental.valid_from = Time.now
-    @rental.valid_to = @rental.valid_from + 2.days
+    @rental = generate_new_rental
     @rental.movie = @movie
     @rental.user = current_user
 
     if @rental.save
       redirect_to @rental, notice: "You have successfully rented #{@movie.name}"
     else
-      redirect_to @movies, notice: "There was an error performing this operation. #{@rental.errors.first.last}"
+      redirect_to movies_path, notice: "There was an error performing the operation. #{@rental.errors.first.last}"
     end
+
   end
 
   private
-  #Use callbacks to share common setup or constraints between actions.
+  
   def set_movie
     @movie = Movie.find(params[:movie_id])
   end
+  
+  def generate_new_rental
+    rental = Rental.new
+    rental.valid_from = Time.now
+    rental.valid_to = rental.valid_from + 2.days
+    rental
+  end
 
+  def verify_if_admin_and_redirect_with_error_message_if_yes
+    if current_user.is_admin?
+      flash[:danger] = "You are not authorized to perform this action"
+      redirect_to movies_url
+    end
+  end
+  
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,17 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  validate :not_currently_rented
+  validates :valid_to, presence: true
+  validates :valid_from, presence: true
+  scope :active, -> {where("valid_from < ? AND valid_to > ?", Time.current, Time.current)}
+
+ def not_currently_rented
+  errors.add(:rental, "You already have an active rental for this Movie") if currently_rented?
+ end
+
+ def currently_rented?
+  Rental.active.where(user: user, movie: movie).count > 0
+ end
+
 end
