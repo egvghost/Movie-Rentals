e11b4926b810fa19844ebea01dcd2b96b46e624f
egvghost <egvghost@gmail.com>
2019-06-05 18:48:42 -0300

    'scope:inactive' added

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -5,6 +5,7 @@ class Rental < ApplicationRecord
   validates :valid_to, presence: true
   validates :valid_from, presence: true
   scope :active, -> {where("valid_from < ? AND valid_to > ?", Time.current, Time.current)}
+  scope :inactive, -> {where("valid_from > ? OR valid_to < ?", Time.current, Time.current)}
 
  def not_currently_rented
   errors.add(:rental, "You already have an active rental for this Movie") if currently_rented?
